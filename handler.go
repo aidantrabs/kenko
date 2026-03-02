84a5fa632707be03be41c8d4c79ebe3990b32eac
Aidan Traboulay <aidantraboulay13@gmail.com>
2026-03-02 14:13:31 -0400

    fix: handle json encode error

--- handler.go
+++ handler.go
@@ -29,7 +29,7 @@ type targetResult struct {
 func writeJSON(w http.ResponseWriter, code int, v any) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
-	json.NewEncoder(w).Encode(v)
+	_ = json.NewEncoder(w).Encode(v)
 }
 
 func HandleHealth(checker *Checker) http.HandlerFunc {
